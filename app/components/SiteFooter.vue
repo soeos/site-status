4c2ec2e436cd032d68284946a98eb730be19ab06
Camp Bill <139830735+soeos@users.noreply.github.com>
2025-05-16 11:52:17 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/soeos/site-status",
+  home: "https://blog.ortech.nyc.mn",
+  email: "mailto:admin@ortech.nyc.mn",
 };
 </script>
 
